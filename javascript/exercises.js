ca93d2d87c742b8fa203197af387d3876d2cdc68
Daniel <daniel.munoz.me1@gmail.com>
2024-09-11 11:46:31 -0700

    js powerGenerator + firstThenLowerCase completed

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -16,8 +16,25 @@ export function change(amount) {
 }
 
 // Write your first then lower case function here
+export function firstThenLowerCase(arr, predicate) {
+  const element = arr.find(predicate)
+
+  // only if exists, if not return undefined
+  return element?.toLowerCase()
+}
 
 // Write your powers generator here
+export function* powersGenerator({ ofBase, upTo }) {
+  let power = 0
+  let value = Math.pow(ofBase, power)
+
+  // yield until limit reached
+  while (value <= upTo) {
+    yield value
+    power += 1
+    value = Math.pow(ofBase, power)
+  }
+}
 
 // Write your say function here
 
--- javascript/exercises.test.js
+++ javascript/exercises.test.js
@@ -2,9 +2,9 @@ import { describe, it } from "node:test"
 import { deepEqual, throws, rejects } from "node:assert/strict"
 import {
   change,
-  // firstThenLowerCase,
+  firstThenLowerCase,
   // say,
-  // powersGenerator,
+  powersGenerator,
   // meaningfulLineCount,
   // Quaternion,
 } from "./exercises.js"
@@ -35,22 +35,22 @@ describe("The change function", () => {
 
 // Uncomment the following tests as you complete the exercises
 
-// describe("The firstThenLowerCase function", () => {
-//   const nonEmpty = (s) => s !== ""
-//   const lengthGreaterThan3 = (s) => s.length > 3
-//   it("works", () => {
-//     deepEqual(firstThenLowerCase([], nonEmpty), undefined)
-//     deepEqual(firstThenLowerCase(["", "A", "B"], nonEmpty), "a")
-//     deepEqual(
-//       firstThenLowerCase(["", "A", "ABC"], lengthGreaterThan3),
-//       undefined
-//     )
-//     deepEqual(
-//       firstThenLowerCase(["ABC", "ABCD", "ABCDE"], lengthGreaterThan3),
-//       "abcd"
-//     )
-//   })
-// })
+describe("The firstThenLowerCase function", () => {
+  const nonEmpty = (s) => s !== ""
+  const lengthGreaterThan3 = (s) => s.length > 3
+  it("works", () => {
+    deepEqual(firstThenLowerCase([], nonEmpty), undefined)
+    deepEqual(firstThenLowerCase(["", "A", "B"], nonEmpty), "a")
+    deepEqual(
+      firstThenLowerCase(["", "A", "ABC"], lengthGreaterThan3),
+      undefined
+    )
+    deepEqual(
+      firstThenLowerCase(["ABC", "ABCD", "ABCDE"], lengthGreaterThan3),
+      "abcd"
+    )
+  })
+})
 
 // describe("The say function", () => {
 //   it("works when there are no words", () => {
@@ -74,21 +74,21 @@ describe("The change function", () => {
 //   })
 // })
 
-// describe("The powers generator", () => {
-//   it("works as expected", () => {
-//     const g1 = powersGenerator({ ofBase: 2, upTo: 1 })
-//     deepEqual(g1.next(), { value: 1, done: false })
-//     deepEqual(g1.next(), { value: undefined, done: true })
-//     const g2 = powersGenerator({ ofBase: 3, upTo: 100 })
-//     deepEqual(g2.next(), { value: 1, done: false })
-//     deepEqual(g2.next(), { value: 3, done: false })
-//     deepEqual(g2.next(), { value: 9, done: false })
-//     deepEqual(g2.next(), { value: 27, done: false })
-//     deepEqual(g2.next(), { value: 81, done: false })
-//     deepEqual(g2.next(), { value: undefined, done: true })
-//     deepEqual([...powersGenerator({ ofBase: 3, upTo: 27 })], [1, 3, 9, 27])
-//   })
-// })
+describe("The powers generator", () => {
+  it("works as expected", () => {
+    const g1 = powersGenerator({ ofBase: 2, upTo: 1 })
+    deepEqual(g1.next(), { value: 1, done: false })
+    deepEqual(g1.next(), { value: undefined, done: true })
+    const g2 = powersGenerator({ ofBase: 3, upTo: 100 })
+    deepEqual(g2.next(), { value: 1, done: false })
+    deepEqual(g2.next(), { value: 3, done: false })
+    deepEqual(g2.next(), { value: 9, done: false })
+    deepEqual(g2.next(), { value: 27, done: false })
+    deepEqual(g2.next(), { value: 81, done: false })
+    deepEqual(g2.next(), { value: undefined, done: true })
+    deepEqual([...powersGenerator({ ofBase: 3, upTo: 27 })], [1, 3, 9, 27])
+  })
+})
 
 // describe("The meaningfulLineCount function", async () => {
 //   await it("throws if no such file", async () => {
