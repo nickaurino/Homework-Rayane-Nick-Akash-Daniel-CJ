abb2e22ddd0c789333003c4aa35759f036312472
Daniel <daniel.munoz.me1@gmail.com>
2024-11-22 16:08:51 -0800

    final changes/clean up code

--- cpp/stack.h
+++ cpp/stack.h
@@ -21,32 +21,28 @@ using namespace std;
 
 template <typename T>
 class Stack {
-
-// // best practices for information hiding
-// private:
-//   size_t capacity;                          // Current capacity of the stack
-//   size_t top;                               // Index of the next available slot
-//   std::unique_ptr<T[]> elements;           // Smart pointer to the stack elements
+// Kept in a private field for best information hiding, put at the top for clarity
+private:
+  size_t capacity;                          // Current capacity of the stack
+  size_t top;                               // Index of the next available slot
+  std::unique_ptr<T[]> elements;           // Smart pointer to the stack elements
 
 public:
-  // Write your stack constructor here
+  // Constructor
   Stack() : capacity(INITIAL_CAPACITY), top(0), elements(std::make_unique<T[]>(INITIAL_CAPACITY)) {}
   // Write your size() method here
   size_t size() const {
     return top;
   }
 
-  // Write your is_empty() method here
   bool is_empty() const {
     return top == 0;
   }
 
-  // Write your is_full() method here
   bool is_full() const {
     return capacity == MAX_CAPACITY && top == capacity;
   }
 
-  // Write your push() method here
   void push(const T& value) {
     if (top == capacity) {
       if (capacity == MAX_CAPACITY) {
@@ -57,7 +53,6 @@ public:
     elements[top++] = value;
   }
 
-  // Write your pop() method here
   T pop() {
     if (is_empty()) {
       throw std::underflow_error("cannot pop from empty stack");
@@ -73,13 +68,6 @@ private:
   size_t capacity;                          // Current capacity of the stack
   size_t top;                               // Index of the next available slot
   std::unique_ptr<T[]> elements;           // Smart pointer to the stack elements
-
-  // We recommend you make a PRIVATE reallocate method here. It should
-  // ensure the stack capacity never goes above MAX_CAPACITY or below
-  // INITIAL_CAPACITY. Because smart pointers are involved, you will need
-  // to use std::move() to transfer ownership of the new array to the stack
-  // after (of course) copying the elements from the old array to the new
-  // array with std::copy().
 
   void reallocate(size_t new_capacity) {
     if (new_capacity > MAX_CAPACITY || new_capacity < INITIAL_CAPACITY) {
